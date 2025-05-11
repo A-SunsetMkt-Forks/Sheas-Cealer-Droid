ca93ddb3269174c92911df7f584f714a934a298b
Space Time <Zeus6_6@163.com>
2025-05-11 22:15:11 +0800

    0.0.2.25

--- Consts/SettingsConst.cs
+++ Consts/SettingsConst.cs
@@ -5,4 +5,6 @@ internal abstract class SettingsConst : SettingsMultilangConst
     internal static string[] ThemeColorNameArray => [.. GlobalConst.ThemeColorDictionary.Keys];
     internal static string[] ThemeStateNameArray => [.. GlobalConst.ThemeStateDictionary.Keys];
     internal static string[] LangOptionNameArray => [.. GlobalConst.LangOptionDictionary.Keys];
+
+    internal static string UpstreamUrlRegexPattern => "^(https?:\\/\\/)?[a-zA-Z0-9](-*[a-zA-Z0-9])*(\\.[a-zA-Z0-9](-*[a-zA-Z0-9])*)*(:\\d{1,5})?(\\/[a-zA-Z0-9.\\-_\\~\\!\\$\\&\\'\\(\\)\\*\\+\\,\\;\\=\\:\\@\\%]*)*$";
 }
