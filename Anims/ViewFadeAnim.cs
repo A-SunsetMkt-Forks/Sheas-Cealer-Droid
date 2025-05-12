1d02c265104e812c141baf99212f83efc2d34ec4
Space Time <Zeus6_6@163.com>
2025-05-12 10:52:02 +0800

    0.0.2.26

--- Anims/ViewFadeAnim.cs
+++ Anims/ViewFadeAnim.cs
@@ -7,17 +7,17 @@ internal class ViewFadeAnim : Animation
 {
     internal enum FadeType { In, Out }
 
-    internal ViewFadeAnim(View fadeInView, FadeType fadeType)
+    internal ViewFadeAnim(View fadeView, FadeType fadeType)
     {
         if (fadeType == FadeType.In)
         {
-            Add(0, 1, new Animation(v => fadeInView.Opacity = v, 0, 1, Easing.CubicInOut));
-            Add(0, 1, new Animation(v => fadeInView.Scale = v, 0.9, 1, Easing.CubicInOut));
+            Add(0, 1, new Animation(v => fadeView.Opacity = v, 0, 1, Easing.CubicInOut));
+            Add(0, 1, new Animation(v => fadeView.Scale = v, 0.9, 1, Easing.CubicInOut));
         }
         else
         {
-            Add(0, 1, new Animation(v => fadeInView.Opacity = v, 1, 0, Easing.CubicInOut));
-            Add(0, 1, new Animation(v => fadeInView.Scale = v, 1, 0.9, Easing.CubicInOut));
+            Add(0, 1, new Animation(v => fadeView.Opacity = v, 1, 0, Easing.CubicInOut));
+            Add(0, 1, new Animation(v => fadeView.Scale = v, 1, 0.9, Easing.CubicInOut));
         }
     }
 }
